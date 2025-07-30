481ce8ca92924f629d036c580709b53874b63acf
maybeags <suren0709@gmail.com>
2025-07-30 12:19:17 +0900

    feat: 20250730 test item for the conditional sentence uploaded

---
+++ src/main/java/ch06_condition/Condition05.java
@@ -0,0 +1,19 @@
+package ch06_condition;
+
+public class Condition05 {
+    public static void main(String[] args) {
+        /*
+            사용자에게 score를 입력 받아 다음과 같은 조건을 만족시키도록 작성하시오.
+            score가 90점 이상이라면 grade는 A
+            score가 80점 이상이라면 grade는 B
+            score가 70점 이상이라면 grade는 C
+            score가 60점 이상이라면 grade는 D
+            score가 50점 이하라면 grade는 F
+
+            실행 예
+            점수를 입력하세요 >>> 68
+            당신의 점수는 68점이고, 학점은 D입니다.
+
+         */
+    }
+}
