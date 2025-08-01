c7ce11c920c65a6bbac165ef40bf0ec8920c45b1
maybeags <suren0709@gmail.com>
2025-08-01 15:18:40 +0900

    student task guideline uploaded

--- src/main/java/ch09_classes/CarMain.java
+++ src/main/java/ch09_classes/CarMain.java
@@ -21,5 +21,33 @@ public class CarMain {
     yourCar는 brake() 메서드 호출
     myCar, yourCar에 각각 displayInfo() 메서드 호출하시오.
      */
+
+        // 객체 생성
+        Car myCar = new Car();
+        // 필드에 값 대입
+        myCar.color = "빨강";
+        myCar.speed = 160;
+        // 객체 생성 2
+        Car yourCar = new Car();
+        yourCar.color = "노랑";
+        yourCar.speed = 180;
+
+        // 메서드 호출
+        myCar.drive();
+        yourCar.drive();
+        myCar.brake();
+        yourCar.brake();
+        myCar.displayInfo();
+        yourCar.displayInfo();
+
+// Constructor 클래스 생성
+// ConstructorMain 클래스 생성
+
+
+
+
+
+
+
     }
 }
