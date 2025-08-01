ac64c190a0b765d13907dd2f6275df20f123c49c
maybeags <suren0709@gmail.com>
2025-08-01 12:04:45 +0900

    feat: makeup material

--- src/main/java/ch08_methods/ScoreCalc03.java
+++ src/main/java/ch08_methods/ScoreCalc03.java
@@ -4,13 +4,15 @@ import java.util.Scanner;
 
 public class ScoreCalc03 {
     public static void main(String[] args) {
-        ScoreCalc02.calcSumAndAvg();
-        Scanner scanner = new Scanner(System.in);
-        scanner.nextInt();
+//        ScoreCalc02.calcSumAndAvg();
+//        Scanner scanner = new Scanner(System.in);
+//        scanner.nextInt();
 
 
         // ch09_classes
         // ClassA 파일 생성
         // ClassAMain 파일 생성
+
+
     }
 }
