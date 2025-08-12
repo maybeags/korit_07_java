ba7b8a066eecdf86bb88229fa356eb17058488fc
maybeags <suren0709@gmail.com>
2025-08-12 11:13:01 +0900

    feat: singleton 삼성 프레임

--- src/main/java/ch18_static/singleton/Samsung.java
+++ src/main/java/ch18_static/singleton/Samsung.java
@@ -1,4 +1,19 @@
 package ch18_static.singleton;
-
+/*
+    방금 작성한 ProductView와 인터넷에서의 레퍼런스를 참조하여
+    Samsung 클래스의 싱글톤 패턴을 작성하시오.
+ */
 public class Samsung {
+    // 정적 필드
+    private static Samsung instance;
+
+    // private으로 NoArgsConstructor 정의
+    private Samsung() {}
+
+    public static Samsung getInstance() {
+        if(instance == null) {
+            instance = new Samsung();
+        }
+        return instance;
+    }
 }
